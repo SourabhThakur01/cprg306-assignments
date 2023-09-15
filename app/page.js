0872605378591306baaadf06495b31a5e71062bf
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-09-15 14:46:38 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function StudentInfo(){
     return(
         <div>
-            <h1>CPRG 306: Web Development 2 - Assignments</h1>
             <p>Name: Sourabh Thakur</p>
+            
         </div>
     )
 }
