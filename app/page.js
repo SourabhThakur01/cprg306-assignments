fbf2041739e8ddf7779d331a8265cdb007945a91
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-09-15 14:55:00 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function StudentInfo(){
     return(
         <div>
             <p>Name: Sourabh Thakur</p>
-            
+            <p>Course section: CPRG 306 A</p>
+            <Link href="https://github.com/SourabhThakur01/cprg306-assignmnets.git">https://github.com</Link>
         </div>
     )
 }
