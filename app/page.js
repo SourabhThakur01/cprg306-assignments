43dfa04179ae63e3edaabc31d2e00e15473e086e
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-09-18 09:58:17 -0700

    add the week2 page

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 export default function StudentInfo(){
     return(
-        <main>
-            <h1 class = "cprg {fontSize: 8xl}">CPRG 306: Web Development 2 - Assignments</h1>
+            <div>
+            <h1 class="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
             <p>Name: Sourabh Thakur</p>
             <p>Course section: CPRG 306 A</p>
-            <Link href="https://github.com/SourabhThakur01/cprg306-assignmnets.git">https://github.com</Link>
-        </main>
+            <Link href="https://github.com/SourabhThakur01/cprg306-assignmnets.git">https://github.com</Link><br/>
+            <Link href="./week2">week 2</Link>
+            </div>
     )
 }
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+
+import StudentInfo from "../page";
+export default function Week2() {
+    return (
+        <div>
+            <h1 class="text-4xl">My Shopping List</h1>
+            <StudentInfo/>
+        </div>
+    )
+}
