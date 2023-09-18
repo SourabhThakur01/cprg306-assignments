974ce0b097fa37dfd10dd94d540fa62179f36e81
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-09-18 10:55:20 -0700

    improve

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import StudentInfo from "./studentInfo";
     export default function cprg306(){
         return(
-                <div>
+                <div class="text-center border border-sky-500">
                 <h1 class="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
                 <StudentInfo/>
                 <Link href="./week2">week 2</Link>
--- app/week2/page.js
+++ app/week2/page.js
@@ -2,7 +2,7 @@
 import StudentInfo from "../studentInfo";
 export default function Week2() {
     return (
-        <div>
+        <div class="text-center border border-sky-500">
             <h1 class="text-4xl">My Shopping List</h1>
             <StudentInfo/>
         </div>
