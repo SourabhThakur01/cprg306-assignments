da5788df8d0f366515997d857ff62c08ec438d34
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-09-22 23:30:05 -0700

    Update item-list.js

--- app/week3/item-list.js
+++ app/week3/item-list.js
@@ -1,4 +1,5 @@
-export default fuction ItemList(){
+import Item from "./item";
+export default function ItemList(){
     const item1 = {
         name: "milk, 4 L 🥛",
         quantity: 1,
@@ -70,4 +71,68 @@ export default fuction ItemList(){
         quantity: 4,
         category: "household",
       };
+    return(
+        <div>
+          <Item
+          name={item1.name}
+          date={item1.date}
+          location={item1.location}
+          />
+          <Item
+          name={item2.name}
+          date={item2.date}
+          location={item2.location}
+          />
+          <Item
+          name={item3.name}
+          date={item3.date}
+          location={item3.location}
+          />
+          <Item
+          name={item4.name}
+          date={item4.date}
+          location={item4.location}
+          />
+          <Item
+          name={item5.name}
+          date={item5.date}
+          location={item5.location}
+          />
+          <Item
+          name={item6.name}
+          date={item6.date}
+          location={item6.location}
+          />
+          <Item
+          name={item7.name}
+          date={item7.date}
+          location={item7.location}
+          />
+          <Item
+          name={item8.name}
+          date={item8.date}
+          location={item8.location}
+          />
+          <Item
+          name={item9.name}
+          date={item9.date}
+          location={item9.location}
+          />
+          <Item
+          name={item10.name}
+          date={item10.date}
+          location={item10.location}
+          />
+          <Item
+          name={item11.name}
+          date={item11.date}
+          location={item11.location}
+          />
+          <Item
+          name={item12.name}
+          date={item12.date}
+          location={item12.location}
+          />
+        </div>
+    )
 }
