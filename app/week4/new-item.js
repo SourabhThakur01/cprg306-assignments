b7eb40476ccff00b1c2022955f59b1d2a206e8c8
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-10-01 19:10:26 -0700

    test

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -7,40 +7,55 @@ export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
 
-
-const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    const item ={
-        name,
-        quantity,
-        category
+    const handleChangeName = (event) => {
+        setName(event.target.value);
     }
-    console.log(item);
 
-    alert("Name: " + name + "\nQuantity: " + quantity + "\nCategory: " + category);
-    setName("");
-    setQuantity(1);
-    setCategory("produce");
-}
+    const handleChangeQuantity = (event) => {
+        setQuantity(event.target.value);
+    }
 
-return(
-    <form>
-        <input type="text" value={name} onChange={setName} required></input>
-        <input type="number" min={1} max={99} value={quantity} onChange={setQuantity} required></input>
-        <select value={category} onChange={setCategory}>
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="meat">Meat</option>
-            <option value="bakery">Bakery</option>
-            <option value="frozen foods">Frozen Foods</option>
-            <option value="canned goods">Canned Goods</option>
-            <option value="dry goods">Dry Goods</option>
-            <option value="beverages">Beverages</option>
-            <option value="snacks">Snacks</option>
-            <option value="household">Household</option>
-            <option value="other">Other</option>
-        </select>
-    </form>
-)
+    const handleChangeCategory = (event) => {
+        setCategory(event.target.value);
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const item ={
+            name,
+            quantity,
+            category
+        }
+        console.log(item);
+
+        alert("Name: " + name + "\nQuantity: " + quantity + "\nCategory: " + category);
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    }
+
+    return(
+        <main>
+            <form>
+                <input type="text" value={name} onChange={handleChangeName} required/>
+                <input type="number" min={1} max={99} value={quantity} onChange={handleChangeQuantity} required />
+                <select value={category} onChange={handleChangeCategory}>
+                    <option value="produce">Produce</option>
+                    <option value="dairy">Dairy</option>
+                    <option value="meat">Meat</option>
+                    <option value="bakery">Bakery</option>
+                    <option value="frozen foods">Frozen Foods</option>
+                    <option value="canned goods">Canned Goods</option>
+                    <option value="dry goods">Dry Goods</option>
+                    <option value="beverages">Beverages</option>
+                    <option value="snacks">Snacks</option>
+                    <option value="household">Household</option>
+                    <option value="other">Other</option>Category
+                </select>
+                <submit onClick={handleSubmit}>Submit</submit>
+            </form>
+        </main>
+    )
 }
---
+++ app/week4/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return(
+        <div className="bg-grey-100">
+            <NewItem/>
+        </div>
+    )
+}
