26e4e0754af8fc0539ec4230e8d791cd180f8079
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-10-01 18:11:00 -0700

    Update new-item.js

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -23,4 +23,24 @@ const handleSubmit = (event) => {
     setQuantity(1);
     setCategory("produce");
 }
+
+return(
+    <form>
+        <input type="text" value={name} onChange={setName} required></input>
+        <input type="number" min={1} max={99} value={quantity} onChange={setQuantity} required></input>
+        <select value={category} onChange={setCategory}>
+            <option value="produce">Produce</option>
+            <option value="dairy">Dairy</option>
+            <option value="meat">Meat</option>
+            <option value="bakery">Bakery</option>
+            <option value="frozen foods">Frozen Foods</option>
+            <option value="canned goods">Canned Goods</option>
+            <option value="dry goods">Dry Goods</option>
+            <option value="beverages">Beverages</option>
+            <option value="snacks">Snacks</option>
+            <option value="household">Household</option>
+            <option value="other">Other</option>
+        </select>
+    </form>
+)
 }
