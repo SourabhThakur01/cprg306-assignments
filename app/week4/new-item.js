75b910e2ed502571115b8677d4c603bd13ab4c6c
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-10-09 11:53:18 -0700

    Update new-item.js

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -37,25 +37,36 @@ export default function NewItem() {
     }
 
     return(
-        <main>
+        <main className="bg-gray-700 p-40">
+            <div className="text-center">
             <form>
-                <input type="text" value={name} onChange={handleChangeName} required/>
-                <input type="number" min={1} max={99} value={quantity} onChange={handleChangeQuantity} required />
-                <select value={category} onChange={handleChangeCategory}>
-                    <option value="produce">Produce</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="meat">Meat</option>
-                    <option value="bakery">Bakery</option>
-                    <option value="frozen foods">Frozen Foods</option>
-                    <option value="canned goods">Canned Goods</option>
-                    <option value="dry goods">Dry Goods</option>
-                    <option value="beverages">Beverages</option>
-                    <option value="snacks">Snacks</option>
-                    <option value="household">Household</option>
-                    <option value="other">Other</option>Category
+                <label className="block pl-10 mt-20 ">
+                    <span className="">Name:</span>
+                <input type="text" value={name} onChange={handleChangeName} required className="text-black ml-10"/>
+                </label>
+                <label className="block pr-20 mt-5">
+                    <span className="mr-11">Quantity:</span>
+                <input type="number" min={1} max={99} value={quantity} onChange={handleChangeQuantity} required className="text-black pl-11"/>
+                </label>
+                <label className="block pr-20 mt-5 mb-10">
+                    <span className="mr-11 ml-3">Category:</span>
+                <select value={category} onChange={handleChangeCategory} className="text-black ">
+                    <option value="produce" className="text-black">Produce</option>
+                    <option value="dairy" className="text-black">Dairy</option>
+                    <option value="meat" className="text-black">Meat</option>
+                    <option value="bakery" className="text-black">Bakery</option>
+                    <option value="frozen foods" className="text-black">Frozen Foods</option>
+                    <option value="canned goods" className="text-black">Canned Goods</option>
+                    <option value="dry goods" className="text-black">Dry Goods</option>
+                    <option value="beverages" className="text-black">Beverages</option>
+                    <option value="snacks" className="text-black">Snacks</option>
+                    <option value="household" className="text-black">Household</option>
+                    <option value="other" className="text-black">Other</option>Category
                 </select>
-                <submit onClick={handleSubmit}>Submit</submit>
+                </label>
+                    <submit onClick={handleSubmit} className="py-3 px-5 bg-gray-700 hover:bg-gray-500 rounded-md">Submit</submit>
             </form>
+            </div>
         </main>
     )
 }
